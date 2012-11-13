eb5c47258e0a6a2d94ac5400e04375d1bf5f9c8a
Ryan Calderon <rcalderon04@gmail.com>
2012-11-12 19:37:11 -0800

    completed wish_list.rb

--- mid_term/wish_list.rb
+++ mid_term/wish_list.rb
@@ -1,28 +1,13 @@
 class WishList
   include Enumerable
   attr_accessor :wishes
-#  def initialize
-#    @wish = []
-#  end
 
-#  def wishes(*wish)
-#    @wish = wish
-#  end
-#  def wishes(wish)
-#    @wish = wish
-#  end
-  
   def each
-    @wish.each {|w| yield(w)}
-  end
-  
-  def map
-    i = 0
-    each do |e|
-      out = yield(e, i)
-      i += 1
-      out
-    end
-  end
-    
+   i=1
+   @wishes.each do |w|
+     out = "#{i}"+ ". " + w
+     yield(out)
+     i += 1
+   end
+  end  
 end
